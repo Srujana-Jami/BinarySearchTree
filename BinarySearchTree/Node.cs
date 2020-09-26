2719e1b6f60475c6ef26a34f04e8e29fe908ca02
Srujana-Jami <srujana.jami@innroad.com>
2020-09-26 14:10:21 +0530

    Add project files.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public Node lchild;
+        public int info;
+        public Node rchild;
+
+        public Node(int i)
+        {
+            info = i;
+            lchild = null;
+            rchild = null;
+        }
+    }
+}
